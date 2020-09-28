66a8b6bbd036b24659b51a714a557d298e7744ab
Poojajanwalkar <71391743+Poojajanwalkar@users.noreply.github.com>
2020-09-28 10:35:24 +0530

    Update My Day 3 Project: A Simple Earnings Calculator.py
    Change variable name  calculated to total_amount

--- My Day 3 Project: A Simple Earnings Calculator.py
+++ My Day 3 Project: A Simple Earnings Calculator.py
@@ -1,5 +1,5 @@
 emp_name = input("Enter your name: ").strip().title()
 hourly_wage = int(input("Enter your Hourly wage : "))
 total_hours= int(input("How many hours you worked this week : "))
-calculated = str(hourly_wage * total_hours)
-print(f"{emp_name} earned {calculated} this week.")
+total_amount= str(hourly_wage * total_hours)
+print(f"{emp_name} earned {total_amount} this week.")
